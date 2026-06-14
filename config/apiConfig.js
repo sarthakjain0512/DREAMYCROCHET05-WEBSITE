1baa4ea4c6ea577c9358fda3de81f49257327559
Sarthak Jain <sarthakjain107@gmail.com>
2026-06-15 01:08:39 +0530

    Update API_BASE_URL for production

--- config/apiConfig.js
+++ config/apiConfig.js
@@ -1,5 +1,5 @@
 // DreamyCrochet05 - Dynamically generated API Configuration
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL ="https://dreamycrochet05-website.onrender.com";
 
 if (!API_BASE_URL) {
   console.warn("⚠️ [apiConfig] API_BASE_URL is not configured! API requests might fail.");
